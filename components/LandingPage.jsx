2ffd18588f88adc0f2d6723fd1ad2127837bc006
Louis Qian <yuq4@illinois.edu>
2022-07-02 15:54:48 -0500

    fix(punctuations): Escaped punctuations

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -8,22 +8,23 @@ const LandingPage = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <h1 className="py-4 text-white-700">
-            Hi, I'm <span className="text-[#5651e5]">Louis</span>
+            Hi, I&apos;m <span className="text-[#5651e5]">Louis</span>
           </h1>
           <h1 className="py-2 text-white-700">
             A Student / Independent Developer
           </h1>
           <p className="py-4 text-white-600 max-w-[70%] m-auto">
-            "At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            &quot;At vero eos et accusamus et iusto odio dignissimos ducimus qui
             blanditiis praesentium voluptatum deleniti atque corrupti quos
             dolores et quas molestias excepturi sint occaecati cupiditate non
-            provident."
+            provident.&quot;
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
               className="rounded-full shadow-lg shadow-[#5651e57f] p-6 cursor-pointer hover:scale-105 ease-in duration-300"
               href="https://www.linkedin.com/in/yu-qian-661741223/"
               target="_blank"
+              rel="noreferrer"
             >
               <FaLinkedinIn />
             </a>
@@ -31,6 +32,7 @@ const LandingPage = () => {
               className="rounded-full shadow-lg shadow-[#5651e57f] p-6 cursor-pointer hover:scale-105 ease-in duration-300"
               href="https://github.com/louisunlimited"
               target="_blank"
+              rel="noreferrer"
             >
               <FaGithub />
             </a>
@@ -38,6 +40,7 @@ const LandingPage = () => {
               className="rounded-full shadow-lg shadow-[#5651e57f] p-6 cursor-pointer hover:scale-105 ease-in duration-300"
               href="https://www.instagram.com/louissss_yu/"
               target="_blank"
+              rel="noreferrer"
             >
               <AiFillInstagram />
             </a>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -98,7 +98,7 @@ const Navbar = () => {
             </ul>
             <div className="pt-40 ">
               <p className="uppercase tracking-widest text-[#5651e5]">
-                Lect's connect
+                Lect&apos;s connect
               </p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
