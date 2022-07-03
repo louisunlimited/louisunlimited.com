bba1624dfba9bffa8566ecf0c53458a00abb20d3
Louis Qian <yuq4@illinois.edu>
2022-07-03 00:55:35 -0500

    chore(data): sorted imports

--- public/data.jsx
+++ public/data.jsx
@@ -1,3 +1,22 @@
+//social icons
+import { AiFillInstagram } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+export const social = [
+  {
+    icon: <FaLinkedinIn />,
+    href: "https://www.linkedin.com/in/yu-qian-661741223/",
+  },
+  {
+    icon: <FaGithub />,
+    href: "https://github.com/louisunlimited",
+  },
+  {
+    icon: <AiFillInstagram />,
+    href: "https://www.instagram.com/louissss_yu/",
+  },
+];
+
 // skill images
 import SkillSwift from "./assets/swift.svg";
 import SkillNextjs from "./assets/next.js.svg";
