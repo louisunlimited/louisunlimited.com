b31fd38b268717216a2ef2424d19c061a9c18fb2
Louis Qian <yuq4@illinois.edu>
2022-08-11 23:22:52 -0700

    feat(SEO): inital SEO setup

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const SEO = {
+  title: "Louis Qian | Student & Developer",
+  description:
+    "Louis Qian is a student and developer. He is a self-taught developer at the University of Illinois at Urbana-Champaign.",
+  canonical: "https://www.louisunlimited.com",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://www.url.ie/",
+    site_name: "Louis Qian | Developer",
+  },
+};
+
+export default SEO;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import SEO from "../next-seo.config";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
