4776e6265f274c7bfacd3079364507dc206e1060
Louis Qian <yuq4@illinois.edu>
2022-08-14 21:42:23 -0700

    chore: fixed seo-url

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,12 @@
 const SEO = {
   title: "Louis Qian | Student & Developer",
   description:
-    "Louis Qian is a student and developer. He is a self-taught developer at the University of Illinois at Urbana-Champaign.",
+    "Louis Qian is a student and developer. He is a self-taught developer studying at the University of Illinois at Urbana-Champaign.",
   canonical: "https://www.louisunlimited.com",
   openGraph: {
     type: "website",
     locale: "en_IE",
-    url: "https://www.url.ie/",
+    url: "https://www.louisunlimited.com",
     site_name: "Louis Qian | Developer",
   },
 };
