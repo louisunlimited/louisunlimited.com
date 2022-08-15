44cbefd8ef4e047cf66b09fc3cb504fa310fe301
Louis Qian <yuq4@illinois.edu>
2022-08-14 21:41:14 -0700

    fix: navbar on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import SEO from "../next-seo.config";
 import { DefaultSeo } from "next-seo";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <Navbar />
       <Component {...pageProps} />;
     </>
   );
