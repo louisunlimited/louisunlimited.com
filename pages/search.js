fbd32974c96918441a8400d1a20a51fea0247ad7
Louis Qian <yuq4@illinois.edu>
2022-08-24 16:45:48 -0500

    fix(search): resolved comment conflit

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,7 @@ const search = () => {
         title="Louis Qian | Louisearch"
         description="A custom search engine for everything! Fully customizable and private!"
       />
-      // form for search
+      {"// form for search"}
       <div className="max-w-[1100px] mx-auto pt-24 h-[calc(100vh_-_140px)] flex justify-center items-center text-center">
         <div className="">
           <h2 className="text-slate-200">Search Something Here!</h2>
