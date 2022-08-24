1f1497b9a9f5d589cfe8e0eb656ff4c37316d90d
Louis Qian <yuq4@illinois.edu>
2022-08-24 16:39:37 -0500

    feat(search): updated SEO tags

--- pages/search.js
+++ pages/search.js
@@ -1,31 +1,38 @@
 import React from "react";
+import { NextSeo } from "next-seo";
 
 const search = () => {
   return (
-    // form for search
-    <div className="max-w-[1100px] mx-auto pt-24 h-[calc(100vh_-_140px)] flex justify-center items-center text-center">
-      <div className="">
-        <h2 className="text-slate-200">Search Something Here!</h2>
-        <form
-          method="post"
-          action="https://louisearch.com"
-          placeholder="Input something to search..."
-          className="w-full"
-          style={{
-            fontSize: "1.2rem",
-            fontWeight: "400",
-            color: "#333",
-          }}
-        >
-          <input type="text" name="q" />
-          <input
-            type="submit"
-            value="Search"
-            className="cursor-pointer text-white"
-          />
-        </form>
+    <>
+      <NextSeo
+        title="Louis Qian | Louisearch"
+        description="A custom search engine for everything! Fully customizable and private!"
+      />
+      // form for search
+      <div className="max-w-[1100px] mx-auto pt-24 h-[calc(100vh_-_140px)] flex justify-center items-center text-center">
+        <div className="">
+          <h2 className="text-slate-200">Search Something Here!</h2>
+          <form
+            method="post"
+            action="https://louisearch.com"
+            placeholder="Input something to search..."
+            className="w-full"
+            style={{
+              fontSize: "1.2rem",
+              fontWeight: "400",
+              color: "#333",
+            }}
+          >
+            <input type="text" name="q" />
+            <input
+              type="submit"
+              value="Search"
+              className="cursor-pointer text-white"
+            />
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
