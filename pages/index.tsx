f7a9abb75120240ad57f742d7b92b0d03b957026
Louis Qian <yuq4@illinois.edu>
2023-03-03 17:28:49 -0600

    feat: plausible support

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,11 @@ export default function Home() {
     <div>
       <Head>
         <title>Louis Qian | Developer</title>
+        <script
+          defer
+          data-domain="louisunlimited.com"
+          src="http://ec2-3-141-7-122.us-east-2.compute.amazonaws.com/js/script.js"
+        ></script>
       </Head>
 
       <LandingPage />
