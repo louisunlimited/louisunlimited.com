b8d52f87cf36d5a893d1c5b52f8120f0d82a9f6c
Louis Qian <yuq4@illinois.edu>
2023-04-22 23:05:23 -0500

    feat: plausible & custom event monitor (#5)
    * feat: plausible & custom event support
    
    * feat: proxy plausible req

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { withPlausibleProxy } = require("next-plausible");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = withPlausibleProxy({
+  customDomain: "https://analytics.louisunlimited.com",
+})(module.exports);
